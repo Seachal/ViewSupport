99dffa2bfa79107d5fb3a9f654638a26552f7215
Seachal <seachalzhang@gmail.com>
2020-09-17 15:37:13 +0800

    关于不规则view 的点击响应

--- Sample/src/main/java/com/gcssloop/test/retry/RemoteControlMenu.java
+++ Sample/src/main/java/com/gcssloop/test/retry/RemoteControlMenu.java
@@ -21,33 +21,38 @@ import com.gcssloop.view.CustomView;
  * *
  */
 public class RemoteControlMenu extends CustomView {
-//       1.
-//    Region 的区域检测。
-//        2.
-//    Matrix 的坐标映射。
-//
+    //       1.
+    //    Region 的区域检测。
+    //        2.
+    //    Matrix 的坐标映射。
+    //
 
 
     //
-    Path up_p, down_p, left_p, right_p, center_p;
+    Path up_path, down_path, left_path, right_path, center_path;
     //  范围
-    Region up_r, down_r, left_r, right_r, center_r;
+    Region up_region, down_region, left_region, right_region, center_region;
 
     Matrix mMapMatrix = null;
 
-//   sca: 标记
+    //   sca: 标记
     int CENTER = 0;
     int UP = 1;
     int RIGHT = 2;
     int DOWN = 3;
     int LEFT = 4;
+    //  标记
     int touchFlag = -1;
+    //  手指按下的标记
     int currentFlag = -1;
 
     MenuListener mListener = null;
 
+    //    默认的磨灰色
     int mDefauColor = 0xFF4E5268;
+    //    淡橙色
     int mTouchedColor = 0xFFDF9C81;
+
     public RemoteControlMenu(Context context) {
         this(context, null);
     }
@@ -55,16 +60,16 @@ public class RemoteControlMenu extends CustomView {
     public RemoteControlMenu(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        up_p = new Path();
-        down_p = new Path();
-        left_p = new Path();
-        right_p = new Path();
-        center_p = new Path();
-        up_r = new Region();
-        down_r = new Region();
-        left_r = new Region();
-        right_r = new Region();
-        center_r = new Region();
+        up_path = new Path();
+        down_path = new Path();
+        left_path = new Path();
+        right_path = new Path();
+        center_path = new Path();
+        up_region = new Region();
+        down_region = new Region();
+        left_region = new Region();
+        right_region = new Region();
+        center_region = new Region();
 
         mDeafultPaint.setColor(mDefauColor);
         mDeafultPaint.setAntiAlias(true);
@@ -75,6 +80,7 @@ public class RemoteControlMenu extends CustomView {
 
     /**
      * sca: measure 后，还是会发生变化，所以需要用 onSizeChanged 再次确定大小。
+     *
      * @param w
      * @param h
      * @param oldw
@@ -100,42 +106,45 @@ public class RemoteControlMenu extends CustomView {
         float smallSweepAngle = -80;
 
         // 根据视图大小，初始化 Path 和 Region
-        center_p.addCircle(0, 0, 0.2f * minWidth, Path.Direction.CW);
-        center_r.setPath(center_p, globalRegion);
+        center_path.addCircle(0, 0, 0.2f * minWidth, Path.Direction.CW);
+        center_region.setPath(center_path, globalRegion);
 
-        right_p.addArc(bigCircle, -40, bigSweepAngle);
-        right_p.arcTo(smallCircle, 40, smallSweepAngle);
-        right_p.close();
-        right_r.setPath(right_p, globalRegion);
+        right_path.addArc(bigCircle, -40, bigSweepAngle);
+        right_path.arcTo(smallCircle, 40, smallSweepAngle);
+        right_path.close();
+        right_region.setPath(right_path, globalRegion);
 
-        down_p.addArc(bigCircle, 50, bigSweepAngle);
-        down_p.arcTo(smallCircle, 130, smallSweepAngle);
-        down_p.close();
-        down_r.setPath(down_p, globalRegion);
+        down_path.addArc(bigCircle, 50, bigSweepAngle);
+        down_path.arcTo(smallCircle, 130, smallSweepAngle);
+        down_path.close();
+        down_region.setPath(down_path, globalRegion);
 
-        left_p.addArc(bigCircle, 140, bigSweepAngle);
-        left_p.arcTo(smallCircle, 220, smallSweepAngle);
-        left_p.close();
-        left_r.setPath(left_p, globalRegion);
+        left_path.addArc(bigCircle, 140, bigSweepAngle);
+        left_path.arcTo(smallCircle, 220, smallSweepAngle);
+        left_path.close();
+        left_region.setPath(left_path, globalRegion);
 
-        up_p.addArc(bigCircle, 230, bigSweepAngle);
-        up_p.arcTo(smallCircle, 310, smallSweepAngle);
-        up_p.close();
-        up_r.setPath(up_p, globalRegion);
+        up_path.addArc(bigCircle, 230, bigSweepAngle);
+        up_path.arcTo(smallCircle, 310, smallSweepAngle);
+        up_path.close();
+        up_region.setPath(up_path, globalRegion);
 
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         float[] pts = new float[2];
-        pts[0] = event.getRawX();
-        pts[1] = event.getRawY();
+//        pts[0] = event.getRawX();
+//        pts[1] = event.getRawY();
+        pts[0] = event.getX();
+        pts[1] = event.getY();
         mMapMatrix.mapPoints(pts);
 
         int x = (int) pts[0];
         int y = (int) pts[1];
 
         switch (event.getActionMasked()) {
+            //从日常逻辑上来说，也是，按下+离开，才构成一次点击事件。 down 后，位置会发生位移，那么要确定按压和离开是不是在同一个区域
             case MotionEvent.ACTION_DOWN:
                 touchFlag = getTouchedPath(x, y);
                 currentFlag = touchFlag;
@@ -165,22 +174,27 @@ public class RemoteControlMenu extends CustomView {
                 touchFlag = currentFlag = -1;
                 break;
         }
-
+        // sca: 点击后，请求重新绘制调用 onDraw
         invalidate();
         return true;
     }
 
-    // 获取当前触摸点在哪个区域
+    /* // 获取当前触摸点在哪个区域  ,
+     *
+     * sca：是个重要方法，触摸点是否被某个Region contains
+     *
+     *
+     * */
     int getTouchedPath(int x, int y) {
-        if (center_r.contains(x, y)) {
+        if (center_region.contains(x, y)) {
             return 0;
-        } else if (up_r.contains(x, y)) {
+        } else if (up_region.contains(x, y)) {
             return 1;
-        } else if (right_r.contains(x, y)) {
+        } else if (right_region.contains(x, y)) {
             return 2;
-        } else if (down_r.contains(x, y)) {
+        } else if (down_region.contains(x, y)) {
             return 3;
-        } else if (left_r.contains(x, y)) {
+        } else if (left_region.contains(x, y)) {
             return 4;
         }
         return -1;
@@ -189,33 +203,35 @@ public class RemoteControlMenu extends CustomView {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        //        sca: 坐标起始点（0,0）平移到此view中心位置
         canvas.translate(mViewWidth / 2, mViewHeight / 2);
 
-        // 获取测量矩阵(逆矩阵)
+        // 获取测量矩阵(逆矩阵)  ， 因为画布的坐标不是左上角，移动到了 view 的中心点，所以 Region 坐标计算也要平移？
         if (mMapMatrix.isIdentity()) {
             canvas.getMatrix().invert(mMapMatrix);
         }
 
         // 绘制默认颜色
-        canvas.drawPath(center_p, mDeafultPaint);
-        canvas.drawPath(up_p, mDeafultPaint);
-        canvas.drawPath(right_p, mDeafultPaint);
-        canvas.drawPath(down_p, mDeafultPaint);
-        canvas.drawPath(left_p, mDeafultPaint);
+        canvas.drawPath(center_path, mDeafultPaint);
+        canvas.drawPath(up_path, mDeafultPaint);
+        canvas.drawPath(right_path, mDeafultPaint);
+        canvas.drawPath(down_path, mDeafultPaint);
+        canvas.drawPath(left_path, mDeafultPaint);
 
         // 绘制触摸区域颜色
         mDeafultPaint.setColor(mTouchedColor);
         if (currentFlag == CENTER) {
-            canvas.drawPath(center_p, mDeafultPaint);
+            canvas.drawPath(center_path, mDeafultPaint);
         } else if (currentFlag == UP) {
-            canvas.drawPath(up_p, mDeafultPaint);
+            canvas.drawPath(up_path, mDeafultPaint);
         } else if (currentFlag == RIGHT) {
-            canvas.drawPath(right_p, mDeafultPaint);
+            canvas.drawPath(right_path, mDeafultPaint);
         } else if (currentFlag == DOWN) {
-            canvas.drawPath(down_p, mDeafultPaint);
+            canvas.drawPath(down_path, mDeafultPaint);
         } else if (currentFlag == LEFT) {
-            canvas.drawPath(left_p, mDeafultPaint);
+            canvas.drawPath(left_path, mDeafultPaint);
         }
+        //        sca: 恢复默认颜色， mDefauColor，
         mDeafultPaint.setColor(mDefauColor);
     }
 
@@ -235,4 +251,5 @@ public class RemoteControlMenu extends CustomView {
 
         void onLeftCliched();
     }
+
 }
